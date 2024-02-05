0efa7c8df6fa23ea6aaa55e89bf40eed7e48740d
Carlos Lima <tec.carlos.lima@gmail.com>
2024-02-05 15:31:20 -0300

    ATUALIZAÇÃO E CONEXAO COM O SHEETS

---
+++ lib/src/models/resources/FildsText.dart
@@ -0,0 +1,12 @@
+class FildsText{
+  static const String carimbo = 'Carimbo de data/hora';
+  static const String auditor = 'Carimbo de data/hora';
+  static const String rua = 'Carimbo de data/hora';
+  static const String bloco = 'Carimbo de data/hora';
+  static const String descricao = 'Carimbo de data/hora';
+  static const String ean = 'Carimbo de data/hora';
+  static const String quantidade = 'Carimbo de data/hora';
+  static const String validade = 'Carimbo de data/hora';
+
+  static List<String> getFilds() => [carimbo,auditor,rua,bloco,descricao,ean,quantidade,validade];
+}
